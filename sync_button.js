6ca443484f02e8044fdbce5459a10734f1c54500
lee-1980 <63563638+lee-1980@users.noreply.github.com>
2022-03-27 13:59:32 -0400

    Create sync_button.js

---
+++ sync_button.js
@@ -0,0 +1,40 @@
+        // example data
+        let farmd_Id = 1;
+        let level = 1;
+        let balance = 0.004
+
+        let metadata = {
+            fields: [
+                { seed: true, seedType: "wheat", status: 3},
+                { seed: true, seedType: "wheat", status: 0},
+                { seed: true, seedType: "wheat", status: 1}
+            ]
+        }
+        
+        await sync_blochchain(farmd_Id, level, balance, metadata);
+
+
+        // response format will be same as withdraw functions
+
+
+    const sync_blochchain = (farm_Id, level, balance, metadata) => {
+        return new Promise(async (resolve)=>{
+            try{
+                let fields = metadata.fields? metadata.fields: [];
+                // connect metamask wallet to site
+                let walletaddress = await metaMask_connect();
+
+                console.log(hex_converter(balance/TokenUSD()));
+                await contractInstance.methods.updateFarmStatus( farm_Id, level, hex_converter(balance/TokenUSD()), fields).send({from: walletaddress});
+                resolve({
+                    result: true
+                });
+            }
+            catch (e){
+                resolve({
+                    result: false,
+                    message: e.message
+                });
+            }
+        });
+    }
