846ccb3f763fd92e1421f961a7fd2d0d83f25b93
lee-1980 <63563638+lee-1980@users.noreply.github.com>
2022-03-27 13:48:01 -0400

    Update JS_for_payment_bnb.js

--- JS_for_payment_bnb.js
+++ JS_for_payment_bnb.js
@@ -1,536 +1,82 @@
+    let contract_address = config.contract_address;
+    let contract_API = config.contract_API;
 
-let contract_address = '0x5098Ddadf1C3c93573e343E16259bBe422157E05';
-let contract_API = [
-    {
-        "inputs": [],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-    },
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "owner",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "approved",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-            }
-        ],
-        "name": "Approval",
-        "type": "event"
-    },
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "owner",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "operator",
-                "type": "address"
-            },
-            {
-                "indexed": false,
-                "internalType": "bool",
-                "name": "approved",
-                "type": "bool"
-            }
-        ],
-        "name": "ApprovalForAll",
-        "type": "event"
-    },
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "previousOwner",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "newOwner",
-                "type": "address"
-            }
-        ],
-        "name": "OwnershipTransferred",
-        "type": "event"
-    },
-    {
-        "anonymous": false,
-        "inputs": [
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "from",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-            },
-            {
-                "indexed": true,
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-            }
-        ],
-        "name": "Transfer",
-        "type": "event"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-            },
-            {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-            }
-        ],
-        "name": "approve",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "owner",
-                "type": "address"
-            }
-        ],
-        "name": "balanceOf",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-            }
-        ],
-        "name": "getApproved",
-        "outputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "owner",
-                "type": "address"
-            },
-            {
-                "internalType": "address",
-                "name": "operator",
-                "type": "address"
-            }
-        ],
-        "name": "isApprovedForAll",
-        "outputs": [
-            {
-                "internalType": "bool",
-                "name": "",
-                "type": "bool"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "mintNFTs",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "payable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "owner",
-                "type": "address"
-            }
-        ],
-        "name": "mint_status",
-        "outputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "name",
-        "outputs": [
-            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "owner",
-        "outputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-            }
-        ],
-        "name": "ownerOf",
-        "outputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "renounceOwnership",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "from",
-                "type": "address"
-            },
-            {
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-            },
-            {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-            }
-        ],
-        "name": "safeTransferFrom",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "from",
-                "type": "address"
-            },
-            {
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-            },
-            {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-            },
-            {
-                "internalType": "bytes",
-                "name": "_data",
-                "type": "bytes"
-            }
-        ],
-        "name": "safeTransferFrom",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "operator",
-                "type": "address"
-            },
-            {
-                "internalType": "bool",
-                "name": "approved",
-                "type": "bool"
-            }
-        ],
-        "name": "setApprovalForAll",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "bytes4",
-                "name": "interfaceId",
-                "type": "bytes4"
-            }
-        ],
-        "name": "supportsInterface",
-        "outputs": [
-            {
-                "internalType": "bool",
-                "name": "",
-                "type": "bool"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "symbol",
-        "outputs": [
-            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-            }
-        ],
-        "name": "tokenURI",
-        "outputs": [
-            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "from",
-                "type": "address"
-            },
-            {
-                "internalType": "address",
-                "name": "to",
-                "type": "address"
-            },
-            {
-                "internalType": "uint256",
-                "name": "tokenId",
-                "type": "uint256"
-            }
-        ],
-        "name": "transferFrom",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "newOwner",
-                "type": "address"
-            }
-        ],
-        "name": "transferOwnership",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "withdraw",
-        "outputs": [],
-        "stateMutability": "payable",
-        "type": "function"
-    }
-];
+    const providerURL = 'https://data-seed-prebsc-1-s1.binance.org:8545/';
+    const web3 = new Web3(window.ethereum);
+    const contractInstance = new web3.eth.Contract(contract_API, contract_address);
+    const chainId = '0x61';
 
 
-const providerURL = 'https://data-seed-prebsc-1-s1.binance.org:8545';
-const web3 = new Web3(providerURL);
-const contractInstance = new web3.eth.Contract(contract_API, contract_address);
-const chainId = '0x61';
-
-
-const get_farm_id = () => {
-    return new Promise(async (resolve) => {
-        try {
-            let walletaddress = await metaMask_connect();
-            let farm_Id = await contractInstance.methods.mint_status(walletaddress).call();
-
-            if(parseInt(farm_Id) > 0 ){
-                resolve(farm_Id);
+    const get_farm_id = () => {
+        return new Promise(async (resolve) => {
+            try {
+                let walletaddress = await metaMask_connect();
+                let farms = await contractInstance.methods.getFarms().call({from: walletaddress});
+                console.log(farms, 'Farm_land');
+                if(farms.length && parseInt(farms[0].tokenId) > 0 ){
+                    resolve(farms[0].tokenId);
+                }
+                else {
+                    resolve(0);
+                }
             }
-            else {
+            catch (error) {
+                console.log(error.message);
                 resolve(0);
             }
-        }
-        catch (error) {
-            console.log(error.message);
-            resolve(0);
-        }
-    });
+        });
 
-};
-const send_BNB = () => {
-    return new Promise(async (resolve) => {
-        try {
-            // send $1 worth BNB
-            const bnb_price = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=BNBUSDT')
-                .then(data => data.json());
+    };
 
-            // change original amount to amount in wei
-            const value = Number.parseFloat(1 / parseFloat(bnb_price.price)).toFixed(18) * 10 ** 18;
+    const send_BNB = () => {
+        return new Promise(async (resolve) => {
+            try {
+                // send $1 worth BNB
+                const bnb_price = await convertBNBToUSDT();
+
+                // change original amount to amount in wei
+                const value = Number.parseFloat(1 / parseFloat(bnb_price.price)).toFixed(18) * 10 ** 18;
 
 
-            // connect metamask wallet to site
-            let walletaddress = await metaMask_connect();
+                // connect metamask wallet to site
+                let walletaddress = await metaMask_connect();
 
 
-            let method = await contractInstance.methods.mintNFTs();
-            let data = method.encodeABI();
-            let hex_value = '0x' + Math.trunc(value).toString(16);
+                let hex_value = hex_converter(value);
+                let method = await contractInstance.methods.mint().send({from: walletaddress, value: hex_value});
+                let done = false;
+                let rendering = false;
 
-            const gas = 212207;
+                const myInterval = setInterval(async function () {
 
-            const transactionParameters = {
-                to: contract_address, // Required except during contract publications.
-                from: walletaddress, // must match user's active address.
-                chainId: chainId,
-                value: hex_value,
-                data: data,
-                gas: gas.toString(16)
-            };
+                    if(rendering) return;
 
-            const transactionHash = await ethereum.request({
-                method: 'eth_sendTransaction',
-                params: [transactionParameters],
-            });
+                    rendering = true;
 
-            let done = false;
-            let rendering = false;
+                    let farm_Id = await get_farm_id();
 
-            const myInterval = setInterval(async function () {
-
-                if(rendering) return;
-
-                rendering = true;
-
-                let farm_Id = await get_farm_id();
-
-                rendering = false;
-
-                if(farm_Id && !done){
-
-                    console.log(farm_Id);
-
-                    clearInterval(myInterval);
-                    done = true;
-                    // Handle the result
-                    resolve({
-                        result: true,
-                        message: 'success',
-                        hashcode: transactionHash,
-                        farm_Id: farm_Id
-                    });
-                }
-
-            }, 1000);
-
-
-
-        } catch (error) {
-            resolve({
-                result: false,
-                message: error.message
-            });
-        }
-    });
-};
+                    rendering = false;
+                    if(farm_Id && !done){
+                        console.log(farm_Id);
+                        clearInterval(myInterval);
+                        done = true;
+                        // Handle the result
+                        resolve({
+                            result: true,
+                            message: 'success',
+                            hashcode: method.transactionHash,
+                            farm_Id: farm_Id
+                        });
+                    }
+                }, 1000);
+            } catch (error) {
+                resolve({
+                    result: false,
+                    message: error.message
+                });
+            }
+        });
+    };
 
 
 //example for calling pay function function
