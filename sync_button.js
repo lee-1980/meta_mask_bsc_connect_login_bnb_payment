08aa4d9e8b8c25c3fca0e8deb79bc53f91868114
lee-1980 <63563638+lee-1980@users.noreply.github.com>
2022-05-27 15:43:53 -0400

    Update sync_button.js

--- sync_button.js
+++ sync_button.js
@@ -18,24 +18,23 @@
 
 
 const sync_blochchain = (farm_Id, level, balance, metadata) => {
-        return new Promise(async (resolve)=>{
-            try{
-                let metadataObject = JSON.parse(metadata)
-                let fields = metadataObject.fields? metadataObject.fields: [];
-                // connect metamask wallet to site
-                let walletaddress = await metaMask_connect();
+    return new Promise(async (resolve) => {
+        try {
+            let metadataObject = JSON.parse(metadata)
+            let fields = metadataObject.fields ? metadataObject.fields : [];
+            // connect metamask wallet to site
+            let walletaddress = await metaMask_connect();
 
-                console.log(hex_converter(balance));
-                await contractInstance.methods.updateFarmStatus( farm_Id, level, hex_converter(balance * 10 ** 18), fields).send({from: walletaddress});
-                resolve({
-                    result: true
-                });
-            }
-            catch (e){
-                resolve({
-                    result: false,
-                    message: e.message
-                });
-            }
-        });
-    }
+            console.log(hex_converter(balance));
+            await contractInstance.methods.updateFarmStatus(farm_Id, level, hex_converter(balance * 10 ** 9), fields).send({from: walletaddress});
+            resolve({
+                result: true
+            });
+        } catch (e) {
+            resolve({
+                result: false,
+                message: e.message
+            });
+        }
+    });
+}
