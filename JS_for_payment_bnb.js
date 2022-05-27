329fc126ba82626b1bdd27bd07f40b7d33af0369
lee-1980 <63563638+lee-1980@users.noreply.github.com>
2022-05-27 17:26:29 -0400

    Update JS_for_payment_bnb.js

--- JS_for_payment_bnb.js
+++ JS_for_payment_bnb.js
@@ -1,56 +1,54 @@
 
   
+const send_BNB = () => {
+    return new Promise(async (resolve) => {
+        try {
+            // send $1 worth BNB
+            const bnb_price = await convertBNBToUSDT();
 
-    const send_BNB = () => {
-        return new Promise(async (resolve) => {
-            try {
-                // send $1 worth BNB
-                const bnb_price = await convertBNBToUSDT();
-
-                // change original amount to amount in wei
-                const value = Number.parseFloat(1 / parseFloat(bnb_price.price)).toFixed(18) * 10 ** 18;
-
-
-                // connect metamask wallet to site
-                let walletaddress = await metaMask_connect();
+            // change original amount to amount in wei
+            const value = Number.parseFloat(1 / parseFloat(bnb_price.price)).toFixed(18) * 10 ** 18;
 
 
-                let hex_value = hex_converter(value);
-                let method = await contractInstance.methods.mint().send({from: walletaddress, value: hex_value});
-                let done = false;
-                let rendering = false;
+            // connect metamask wallet to site
+            let walletaddress = await metaMask_connect();
 
-                const myInterval = setInterval(async function () {
 
-                    if(rendering) return;
+            let hex_value = hex_converter(value);
+            let method = await contractInstance.methods.mint().send({from: walletaddress, value: hex_value});
+            let done = false;
+            let rendering = false;
 
-                    rendering = true;
+            const myInterval = setInterval(async function () {
 
-                    let farm_Id = await get_farm_id();
+                if (rendering) return;
 
-                    rendering = false;
-                    if(farm_Id && !done){
-                        console.log(farm_Id);
-                        clearInterval(myInterval);
-                        done = true;
-                        // Handle the result
-                        resolve({
-                            result: true,
-                            message: 'success',
-                            hashcode: method.transactionHash,
-                            farm_Id: farm_Id
-                        });
-                    }
-                }, 1000);
-            } catch (error) {
-                resolve({
-                    result: false,
-                    message: error.message
-                });
-            }
-        });
-    };
+                rendering = true;
 
+                let farm_Id = await get_farm_id();
+
+                rendering = false;
+                if (farm_Id && !done) {
+                    console.log(farm_Id);
+                    clearInterval(myInterval);
+                    done = true;
+                    // Handle the result
+                    resolve({
+                        result: true,
+                        message: 'success',
+                        hashcode: method.transactionHash,
+                        farm_Id: farm_Id
+                    });
+                }
+            }, 1000);
+        } catch (error) {
+            resolve({
+                result: false,
+                message: error.message
+            });
+        }
+    });
+};
 
 //example for calling pay function function
 
