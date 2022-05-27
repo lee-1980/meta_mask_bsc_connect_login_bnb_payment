adcd42e68e9c1fd42561a6ed206362310dfb46ee
lee-1980 <63563638+lee-1980@users.noreply.github.com>
2022-05-27 15:41:40 -0400

    Update get_balance_by_Id.js

--- get_balance_by_Id.js
+++ get_balance_by_Id.js
@@ -1,25 +1,22 @@
 
 
-// Just use this function to update game balance!
-
 const get_balance_by_Id = (farm_Id) => {
-     return new Promise(async (resolve) => {
-          try {
-                let walletaddress = await metaMask_connect();
-                let farm = await contractInstance.methods.getFarm(farm_Id).call({from: walletaddress});
-                resolve({
-                    result: true,
-                    balance: farm.balance  / 10 ** 18
-                })
-          }
-          catch (error) {
-               console.log(error.message);
-                resolve({
-                    result: false,
-                    message: error.message
-                });
-          }
-     });
+    return new Promise(async (resolve) => {
+        try {
+            let walletaddress = await metaMask_connect();
+            let farm = await contractInstance.methods.getFarm(farm_Id).call({from: walletaddress});
+            resolve({
+                result: true,
+                balance: farm.balance / 10 ** 9
+            })
+        } catch (error) {
+            console.log(error.message);
+            resolve({
+                result: false,
+                message: error.message
+            });
+        }
+    });
 }
 
 
