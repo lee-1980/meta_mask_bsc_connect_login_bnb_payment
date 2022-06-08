83684dcad2873314bf3564d8ad071e39b9d1acf4
lee-1980 <63563638+lee-1980@users.noreply.github.com>
2022-06-08 04:08:45 -0400

    Update deposit.js

--- deposit.js
+++ deposit.js
@@ -26,4 +26,26 @@ const deposit_token_into = (tokenAmount, tokenId) => {
     })
 }
 
+
+const deposit_token_metamask = (tokenAmount, tokenId) => {
+    return new Promise(async (resolve) => {
+        try {
+            // To be implemented ...
+            let walletaddress = await metaMask_connect();
+            console.log(tokenAmount, 'tokenAMount');
+            console.log(tokenId, 'TokenID');
+            let value = web3.utils.toWei(web3.utils.toBN(tokenAmount), 'gwei');
+            console.log(value);
+            await contractInstance.methods.depositTokenFromWalletIntoFarm(value, tokenId).send({from: walletaddress});
+            resolve({
+                result: true
+            });
+        } catch (e) {
+            resolve({
+                result: false,
+                message: e.message
+            });
+        }
+    });
+}
         
